56acff9068106e510e1a7c8fc7312afe9068d177
Yan Alves <yanalves@hotmail.com>
2020-11-29 16:16:27 -0300

    Updates layout with personal information

---
+++ src/components/Profile/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Profile = () => (
+  <div className="Profile-wrapper">
+    <h1>Yan Alves Monteiro</h1>
+    <h2>Full-stack developer</h2>
+    <p>Programmer based on Rio de Janeiro</p>
+  </div>
+)
+
+export default Profile
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import Layout from '../components/layout'
+import SEO from "../components/seo"
 
 const AboutPage = () => (
-  <>
+  <Layout>
+    <SEO title="About" />
     <h1>About Page</h1>
     <ul>
       <li>
@@ -17,7 +20,7 @@ const AboutPage = () => (
         </Link>
       </li>
     </ul>
-  </>
+  </Layout>
 )
 
 export default AboutPage
