2720901bdd1aaeac3c23bf216d09170d07b647e5
Yan Alves <yanalves@hotmail.com>
2020-11-29 16:08:57 -0300

    Creates navigation menu

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,7 +1,23 @@
 import React from 'react'
+import { Link } from 'gatsby'
 
 const AboutPage = () => (
-  <h1>About Page</h1>
+  <>
+    <h1>About Page</h1>
+    <ul>
+      <li>
+        <a href="/about">about</a>
+      </li>
+      <li>
+        <Link to="/" activeStyle={{ color: 'red' }}>Home</Link>
+      </li>
+      <li>
+        <Link to="/about" activeStyle={{ color: 'red' }}>
+          about(gatsby link)
+        </Link>
+      </li>
+    </ul>
+  </>
 )
 
 export default AboutPage
